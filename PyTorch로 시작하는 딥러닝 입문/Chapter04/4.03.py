228a1446fe58b8fcbbb4883e5955cc330646f0d3
AhnYeonghoo <dksdudgn00@naver.com>
2022-11-07 08:46:05 +0900

    SoftMax Function

--- PyTorch로 시작하는 딥러닝 입문/Chapter04/4.03.py
+++ PyTorch로 시작하는 딥러닝 입문/Chapter04/4.03.py
@@ -1,25 +1,28 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-
+import torch.nn.functional as F
 torch.manual_seed(1)
+
+
+class BinaryClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(2, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.sigmoid(self.linear(x))
 
 x_data = [[1, 2], [2, 3], [3, 1], [4, 3], [5, 3], [6, 2]]
 y_data = [[0], [0], [0], [1], [1], [1]]
 x_train = torch.FloatTensor(x_data)
 y_train = torch.FloatTensor(y_data)
 
-model = nn.Sequential(
-    nn.Linear(2, 1),
-    nn.Sigmoid()
-)
-
-model(x_train)
-
+model = BinaryClassifier()
 optimizer = optim.SGD(model.parameters(), lr=1)
-nb_epochs = 1000
 
+nb_epochs = 1000
 for epoch in range(nb_epochs+1):
     hypothesis = model(x_train)
     cost = F.binary_cross_entropy(hypothesis, y_train)
@@ -31,12 +34,6 @@ for epoch in range(nb_epochs+1):
         prediction = hypothesis >= torch.FloatTensor([0.5])
         correct_prediction = prediction.float() == y_train
         accuracy = correct_prediction.sum().item() / len(correct_prediction)
-        print('Epoch {:4d}/{} Cost: {:.6f} Accuracy {:2.2f}%'.format(
+        print('Epoch {:4d}/{} Cost: {:.6f} Accuracy {:2.2f}%'.format(  # 각 에포크마다 정확도를 출력
             epoch, nb_epochs, cost.item(), accuracy * 100,
         ))
-
-
-
-model(x_train)
-
-print(list(model.parameters()))
