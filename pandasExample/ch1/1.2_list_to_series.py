eb611149efa4feb9e072947f26243c0e2e101034
Yeonghoo_Ahn <77203153+AhnYeonghoo@users.noreply.github.com>
2022-12-29 10:29:08 +0900

    Delete pandasExample directory

--- pandasExample/ch1/1.2_list_to_series.py
+++
@@ -1,9 +0,0 @@
-list_data = ['2019-01-02', 3.14, 'ABC', 100, True]
-lr = pd.Series(list_data)
-print(lr)
-
-idx = lr.index
-val = lr.values
-print(idx)
-print('\n')
-print(val)
--- pandasExample/ch2/2.3_read_json.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_json('./read_json_sample.json')
-print(df);print('\n')
-print(df.index)
